6e201a1773aa3abccb595eb0f1e8dcc1bbad5987
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-04-24 14:51:50 +0200

    added register view

--- diet_manager/urls.py
+++ diet_manager/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from manager.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', RegisterUserView.as_view()),
+    url(r'^login/$', LoginView.as_view()),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 # Create your views here.
+
+class RegisterUserView(CreateView):
+    model = User
+    fields = ['username', 'password']
+    template_name = 'manager/auth/user_form.html'
