762ce6bf20ddfc62207d2f9dea9c6dc49d9bc1bc
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-04-25 19:53:31 +0200

    fixed login & register templates

--- manager/forms.py
+++ manager/forms.py
@@ -1,11 +1,10 @@
 from django import forms
+#from gollum import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
 
 
-class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    email = forms.EmailField(max_length=150)
-    password = forms.CharField(widget=forms.PasswordInput)
+class RegisterForm(LoginForm):
+    email = forms.EmailField(max_length=150, widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}))
