6ea909352df20a788dabfeb11b767fde61b9a37f
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-06-21 13:55:48 +0200

    target form is now working

--- manager/static/js/target.js
+++ manager/static/js/target.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     var btns = document.querySelectorAll(".targets");
     var inputs = document.querySelectorAll("input");
 
-    console.log(inputs);
     for(var i = 0; i < btns.length; i++){
         btns[i].addEventListener("click", function(){
-
+            var target_data = this.getAttribute("data").split(",");
+            for(var j = 1; j < inputs.length; j++){
+                inputs[j].value = target_data[j - 1];
+            };
         });
     };
 });
