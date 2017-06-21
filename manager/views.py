03bb8fff95a7eb1b45d8824ceb8ad28cb7f617cc
Pawel Wyzykowski <pawelwyzykowski@gmail.com>
2017-06-21 14:08:04 +0200

    minor fixes

--- manager/views.py
+++ manager/views.py
@@ -68,7 +68,12 @@ class MainPageView(LoginRequiredMixin, View):
         week_back = current_date - timedelta(days=7)
         meals = Meal.objects.filter(meal_date__gt=week_back, meal_date__lt=current_date)
         today_meals = Meal.objects.filter(meal_date=current_date)
-        all_targets = Target.objects.all()
+        all_targets = Target.objects.filter(created_by=request.user).values('calories',
+                                                                            'carbohydrates',
+                                                                            'protein',
+                                                                            'sugars',
+                                                                            'salt',
+                                                                            'fat').distinct()
         today_eaten = {'calories':0,
                        'carbohydrates':0,
                        'protein':0,
